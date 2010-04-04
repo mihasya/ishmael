dd0aa24ecee2673c5eaa1fce0bb4af828154ef48
mihasya <m@mihasya.com>
2010-04-04 15:05:33 -0700

    closes #2 final cleanup for format_query

--- init.php
+++ init.php
@@ -10,8 +10,11 @@
 	}
 
 	function format_query($query) {
-		$pattern = "/(?i)((\/\*.*?\*\/)|SELECT|FROM|WHERE|(INNER|OUTER|STRAIGHT|RIGHT|LEFT(?:\s?))?JOIN|ORDER BY|GROUP BY|DESC|FORCE INDEX|USE INDEX|BETWEEN|USING|AND|IN|OR|AS|ON)/";
+		$pattern = "/(?i)(\/\*.*?\*\/)|(\b(SELECT|FROM|WHERE|(INNER|OUTER|STRAIGHT|RIGHT|LEFT(?:\s?))?JOIN|ORDER BY|GROUP BY|DESC|FORCE INDEX|USE INDEX|BETWEEN|USING|AND|IN|OR|AS|ON)\b)/";
 		$query = preg_replace_callback($pattern, '_format_query_callback', $query);
+
+		$query = preg_replace("/(\w)\s*,/", "$1, ", $query);
+
 		return "<span class=\"query\">{$query}</span>";
 	}
 
