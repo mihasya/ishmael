aef2fafd36db4392e1ee767ed2313b235a2be838
mihasya <m@mihasya.com>
2010-04-04 13:25:37 -0700

    move code around, touch up some styles

--- dashboard.php
+++ dashboard.php
@@ -1,25 +1,13 @@
 <?php
+	#
+	# main dashboard UI - listing of queries sorted by various characteristics
+	#
+
 	require_once('init.php');
-
-	function _format_query_callback($str) {
-		$fragment_class = "keyword";
-		if (preg_match("/^\s*\/\*.*?\*\/\s*$/", $str[0])) {
-			$fragment_class = "comment";
-		}
-		return "<span class=\"{$fragment_class}\">" . $str[0] . "</span>";
-	}
-
-	function format_query($query) {
-		$pattern = "/((\/\*.*?\*\/)|SELECT|FROM|WHERE|(INNER|OUTER|STRAIGHT|RIGHT|LEFT(?:\s?))?JOIN|AND|OR|ORDER BY|GROUP BY|DESC)/";
-		$query = preg_replace_callback($pattern, '_format_query_callback', $query);
-		return $query;
-	}
 
 	# hours
 	$hours = ($_GET['hours']) ? $_GET['hours'] : 24;
 	$sort = ($_GET['sort']) ? $_GET['sort'] : "ratio";
-	mysql_connect($conf['db_host'],$conf['db_user'],$conf['db_password']);
-	@mysql_select_db($conf['db_database']) or die("Unable to select database");
 
 	# Get total amount of query time (for % later)
 	$q = "SELECT
--- init.php
+++ init.php
@@ -1,2 +1,20 @@
 <?php
 	require_once('conf.php');
+
+	function _format_query_callback($str) {
+		$fragment_class = "keyword";
+		if (preg_match("/^\s*\/\*.*?\*\/\s*$/", $str[0])) {
+			$fragment_class = "comment";
+		}
+		return "<span class=\"{$fragment_class}\">" . $str[0] . "</span>";
+	}
+
+	function format_query($query) {
+		$pattern = "/((\/\*.*?\*\/)|SELECT|FROM|WHERE|(INNER|OUTER|STRAIGHT|RIGHT|LEFT(?:\s?))?JOIN|AND|OR|ORDER BY|GROUP BY|DESC)/";
+		$query = preg_replace_callback($pattern, '_format_query_callback', $query);
+		return $query;
+	}
+
+
+	mysql_connect($conf['db_host'],$conf['db_user'],$conf['db_password']);
+	@mysql_select_db($conf['db_database_mk']) or die("Unable to select database");
