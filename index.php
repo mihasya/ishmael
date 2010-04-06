3f9e1a3174a97cf4e231ffb878fcf294315372da
mihasya <m@mihasya.com>
2010-04-06 13:47:50 -0700

    placeholder index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	#
+	# for now, just redirect to the dashboard until we figure out what should
+	# go here (probably something about picking which DB you want to look at
+	# once we support multiple DBs)
+	#
+
+	include('dashboard.php');
