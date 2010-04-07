f3a506edd7854094e4c94b665072a5d20f66b963
mihasya <m@mihasya.com>
2010-04-06 19:37:27 -0700

    closes #1 add CREATE TABLE output to explain page (includes terrible hack for resolving table aliases)

--- explain.php
+++ explain.php
@@ -16,9 +16,30 @@
 	@mysql_select_db($conf['db_database_live']) or die("Unable to select database");
 	$result = mysql_query($explain);
 	$rows = array();
+
+	$tables = array();
+
 	while ($row = mysql_fetch_assoc($result)) {
 		$row['possible_keys'] = implode('<br />', explode(',', $row['possible_keys']));
 		$rows[] = $row;
+		$table_alias = $row['table'];
+		$create_result = mysql_query("SHOW CREATE TABLE {$table_alias}");
+		if (!$create_result) {
+			# try to figure out the alias
+			$pattern = "/([\w]+?)(?=\s+\b{$table_alias}\b)/";
+			list($columns, $rest) = preg_split("/(?i)from/", $query);
+			preg_match_all($pattern, $rest, $matches);
+			$table = $matches[0][0];
+			$create_result = mysql_query("SHOW CREATE TABLE {$table}");
+			if (!$create_result) {
+				$table = '';
+			}
+		} else {
+			$table = $table_alias;
+		}
+		if ($create_result) {
+			$tables[$table] = mysql_fetch_assoc($create_result);
+		}
 	}
 
 	require("explain.tpl");
--- explain.tpl
+++ explain.tpl
@@ -1,4 +1,5 @@
 <?php include("top.tpl"); ?>
+		<h3>EXPLAIN result</h3>
 		<p>
 			<?= format_query($query) ?>
 		</p>
@@ -13,6 +14,19 @@
 				<? foreach ($row as $key => $value): ?>
 				<td><?= $value ?></td>
 				<? endforeach; ?>
+			</tr>
+			<? endforeach; ?>
+		</table>
+		<h3>Tables</h3>
+		<table>
+			<tr>
+				<th>Table</th>
+				<th>CREATE TABLE</th>
+			</tr>
+			<? foreach ($tables as $table => $info): ?>
+			<tr <? if ($table_counter++ % 2): ?>class="alt"<? endif ?>>
+				<td><?= $table ?></td>
+				<td><?= nl2br($info['Create Table']) ?></td>
 			</tr>
 			<? endforeach; ?>
 		</table>
